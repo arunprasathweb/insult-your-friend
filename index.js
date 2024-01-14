eebb133dd123285fa213554efd4d6d69c13e0080
Arun Prasath <arunselvaraj4002.gmail.com>
2024-01-14 17:00:26 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+
+app.get("/", async(req, res) => {
+    try{
+        const result = await axios.get("https://evilinsult.com/generate_insult.php?lang=en&type=text");
+
+        res.render("index.ejs", {data : result.data});
+        console.log(result.data);
+        }catch (error){
+        res.render("index.ejs", { content: JSON.stringify(error.response)})
+        }
+   });
+
+app.listen(port, () => {
+    console.log("App is up and running");
+    
+});
